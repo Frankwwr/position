50792292b9be2fa22664c168ef55239e7c6affcf
Frankwwr <138426778+Frankwwr@users.noreply.github.com>
2025-08-28 16:56:44 +0800

    Add files via upload

--- include/decode.h
+++ include/decode.h
@@ -18,14 +18,13 @@ Map_result decode_init()
 
     // 用于定位的列表为两个数相乘
     std::list<int> lst1 = {2, 3, 4, 6, 8, 12};
-    std::list<int> lst2 = {30, 35, 40, 42, 48, 56};
 
     Map_result result;
 
-    for (int i = 0; i < 4; i++)
-        for (int j = i + 1; j < 4; j++)
-            for (int k = 0; k < 4; k++)
-                for (int l = k + 1; l < 4; l++)
+    for (int i = 1; i < 5; i++)//此处有问题
+        for (int j = i + 1; j < 5; j++)
+            for (int k = 1; k < 5; k++)
+                for (int l = k + 1; l < 5; l++)
                 {
                     std::vector<int> nor_re;
                     nor_re.push_back(map[i][k]);
@@ -34,12 +33,14 @@ Map_result decode_init()
                     nor_re.push_back(map[j][k]);
                     std::sort(nor_re.begin(), nor_re.end());
                     int l1 = std::distance(lst1.begin(), std::find(lst1.begin(), lst1.end(), i * j)); // 给出两个变量的位置
-                    int l2 = std::distance(lst2.begin(), std::find(lst2.begin(), lst2.end(), k * l));
+                    int l2 = std::distance(lst1.begin(), std::find(lst1.begin(), lst1.end(), k * l));
                     result[l1][l2][0] = nor_re[0];
                     for (int num : nor_re)
                     {
-                        if (num - result[l1][l2][0] == 1)
+                        if (num - result[l1][l2][0] == 1){
                             result[l1][l2][1] = num;
+                            break;
+                        }
                         else
                             result[l1][l2][0] = num;
                     }
@@ -54,10 +55,10 @@ std::array<int, 2> decoode(int x, int y)
 {
     // 用于定位的列表
     std::list<int> lst1 = {2, 3, 4, 6, 8, 12};
-    std::list<int> lst2 = {30, 35, 40, 42, 48, 56};
+
 
     int l1 = std::distance(lst1.begin(), std::find(lst1.begin(), lst1.end(), x)); // 给出两个变量的位置
-    int l2 = std::distance(lst2.begin(), std::find(lst2.begin(), lst2.end(), y));
+    int l2 = std::distance(lst1.begin(), std::find(lst1.begin(), lst1.end(), y));
 
     return map_of_decode[l1][l2];
 }
